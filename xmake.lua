72fe34d0558126bba5a40da4692e7a2d6f38ff18
GochenRyan <rangaocheng@outlook.com>
2023-04-11 18:17:17 +0800

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,10 @@
 target("libFreeType")
     add_includedirs(
-        "./include/"
+        "$(projectdir)/Vendor/freetype/include/"
     )
 
     on_build("windows", function(target)
+        os.cd("$(projectdir)/Vendor/freetype")
         if (is_mode("debug"))
         then
             os.run("cmake -B build -D CMAKE_BUILD_TYPE=Debug CMAKE_CXX_FLAGS=\"/MT\"")
@@ -12,10 +13,17 @@ target("libFreeType")
             os.run("cmake -B build -D CMAKE_BUILD_TYPE=Release CMAKE_CXX_FLAGS=\"/MT\"")
             os.run("cmake --build build")
         end
+        os.cd("$(projectdir)")
     end)
 
     after_build("windows", function(target)
-        os.cp("build/Debug/*.lib", "$(projectdir)/lib/")
+        os.cp("$(projectdir)/Vendor/freetype/build/Debug/*.lib", "$(projectdir)/lib/")
+        os.trymv("$(projectdir)/lib/freetyped.lib", "$(projectdir)/lib/libFreeType.lib")
+        os.trymv("$(projectdir)/lib/freetype.lib", "$(projectdir)/lib/libFreeType.lib")
+
+        os.cp("$(projectdir)/Vendor/freetype/build/Debug/*.pdb", "$(projectdir)/lib/")
+        os.trymv("$(projectdir)/lib/freetyped.pdb", "$(projectdir)/lib/libFreeType.pdb")
+        os.trymv("$(projectdir)/lib/freetype.pdb", "$(projectdir)/lib/libFreeType.pdb")
     end)
 
     on_clean("windows", function(target)
@@ -23,3 +31,4 @@ target("libFreeType")
             os.rm("$(buildir)/")
         end
     end)
+    
